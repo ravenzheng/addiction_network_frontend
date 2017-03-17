f585a5e0c32127ecaa93c0639701dbbe3383b55a
sanjay singh <sanjays442@gmail.com>
2017-03-17 15:53:22 +0530

    Removed the  from Add Listing page

--- app/addListing/ctrl.js
+++ app/addListing/ctrl.js
@@ -1,22 +1,68 @@
-function ctrl($scope, $routeParams, service) {
-    $scope.required = true;
-    $scope.type = $routeParams.type;
-    var id = $routeParams.id;
-    var auth = '';
-    service.getAuthtoken($scope).then(function (response) {
-        auth = response;
-    }).catch(function (err) {
-        console.log('error: ' + err);
-    });
-    var formData = new FormData();
-    $scope.success_msg = 0;
-    $scope.submitForm = function () {
-        service.addTreatmentCenterSignUp($scope, auth, formData).then(function (response) {
-            $scope.success_msg = 1;
+function ctrl($scope, service) {
+    var vm = this;
+    vm.submit = function () {
+        var formData = new FormData();
+        var data_signup = {
+            'email': vm.email,
+            'password': vm.password,
+            'password_confirmation': vm.confirm_password,
+            'first_name': vm.first_name,
+            'last_name': vm.last_name,
+            'company': vm.company,
+            'phone': vm.phone,
+        };
+        for (var key in data_signup) {
+            formData.append('user[' + key + ']', data_signup[key]);
+        }
+        if (vm.center_name != "") {
+            var data_treat = {
+                'center_name': vm.center_name,
+                'description': vm.description,
+                'center_web_link': vm.center_web_link,
+                'listing_image': vm.listing_image,
+                'heading_1': vm.heading_1,
+                'heading_2': vm.heading_2,
+                'heading_3': vm.heading_3,
+                'heading_4': vm.heading_4,
+                'content_1': vm.content_1,
+                'content_2': vm.content_2,
+                'content_3': vm.content_3,
+                'content_4': vm.content_4,
+                'address_line_1': vm.address_line_1,
+                'address_line_2': vm.address_line_2,
+                'city': vm.city,
+                'pincode': vm.pincode,
+                'state': vm.state,
+                'phone': vm.intakephone,
+                'email': vm.intakeemail,
+                'featured': false,
+                'listing_type': 'free'
+            };
+            for (var key in data_treat) {
+                formData.append('treatment_center[' + key + ']', data_treat[key]);
+            }
+        }
+        if (vm.image_data) {
+            var image_data = vm.image_data
+            var len = image_data.length;
+            for (var i = 0; i < len; i++) {
+                formData.append('treatment_center[image_data][]', image_data.item(i));
+            }
+        }
+        var auth = '';
+        service.getAuthtoken().then(function (response) {
+            auth = response;
+        }).catch(function (err) {
+            alert(err);
+        });
+        service.addTreatmentCenterSignUp(auth, formData).then(function (response) {
+            alert("Your Listing has been saved");
+            location.reload(true);
         }).catch(function (err) {
             $("#email_err").html(err.data.user.email.errors);
             $("#pass_err").html(err.data.user.password.errors);
         });
+
     }
 }
-module.exports = ['$scope', '$routeParams', 'addTreatmentCenterSignUpService', ctrl];
+module.exports = ['$scope', 'addTreatmentCenterSignUpService', ctrl];
--- app/addListing/service.js
+++ app/addListing/service.js
@@ -1,53 +1,6 @@
 function service($http, endPoint, UserService) {
     return {
-        addTreatmentCenterSignUp: function ($scope, auth, formData) {
-            var data_signup = {
-                'email': $scope.addtreatmentcenteruser.email,
-                'password': $scope.addtreatmentcenteruser.password,
-                'password_confirmation': $scope.addtreatmentcenteruser.confirm_password,
-                'first_name': $scope.addtreatmentcenteruser.first_name,
-                'last_name': $scope.addtreatmentcenteruser.last_name,
-                'company': $scope.addtreatmentcenteruser.company,
-                'phone': $scope.addtreatmentcenteruser.phone,
-            };
-            for (var key in data_signup) {
-                formData.append('user[' + key + ']', data_signup[key]);
-            }
-            if ($scope.addtreatmentcenter.center_name != "") {
-                var data_treat = {
-                    'center_name': $scope.addtreatmentcenter.center_name,
-                    'description': $scope.addtreatmentcenter.description,
-                    'center_web_link': $scope.addtreatmentcenter.center_web_link,
-                    'listing_image': $scope.addtreatmentcenter.listing_image,
-                    'heading_1': $scope.addtreatmentcenter.heading_1,
-                    'heading_2': $scope.addtreatmentcenter.heading_2,
-                    'heading_3': $scope.addtreatmentcenter.heading_3,
-                    'heading_4': $scope.addtreatmentcenter.heading_4,
-                    'content_1': $scope.addtreatmentcenter.content_1,
-                    'content_2': $scope.addtreatmentcenter.content_2,
-                    'content_3': $scope.addtreatmentcenter.content_3,
-                    'content_4': $scope.addtreatmentcenter.content_4,
-                    'address_line_1': $scope.addtreatmentcenter.address_line_1,
-                    'address_line_2': $scope.addtreatmentcenter.address_line_2,
-                    'city': $scope.addtreatmentcenter.city,
-                    'pincode': $scope.addtreatmentcenter.pincode,
-                    'state': $scope.addtreatmentcenter.state,
-                    'phone': $scope.addtreatmentcenter.phone,
-                    'email': $scope.addtreatmentcenter.email,
-                    'featured': false,
-                    'listing_type':'free'
-                };
-                for (var key in data_treat) {
-                    formData.append('treatment_center[' + key + ']', data_treat[key]);
-                }
-            }
-            if ($scope.addtreatmentcenter.image_data) {
-                var image_data = $scope.addtreatmentcenter.image_data
-                var len = image_data.length;
-                for (var i = 0; i < len; i++) {
-                    formData.append('treatment_center[image_data][]', image_data.item(i));
-                }
-            }
+        addTreatmentCenterSignUp: function (auth, formData) {
             var req = $http({
                 url: endPoint + '/registrations',
                 method: "POST",
@@ -58,7 +11,6 @@ function service($http, endPoint, UserService) {
                     'Content-Type': undefined
                 }
             });
-
             return req.then(function (res) {
                 var statusCode = res.status;
                 return res.data;
@@ -71,11 +23,9 @@ function service($http, endPoint, UserService) {
                 var token = UserService.getToken();
                 return token;
             }).catch(function (error) {
-                console.log(error);
                 throw error;
             });
         }
     };
 }
-
 module.exports = ['$http', 'endPoint', 'UserService', service];
