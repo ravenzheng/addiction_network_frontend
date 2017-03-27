3c5f1a57b27b12de18bf487a13c4a139ced642e5
sanjays442 <sanjays442@gmail.com>
2017-03-27 19:46:14 +0530

    Added scroll top on next page in pagination
    Added scroll top on next page in pagination

--- app/sponsorAds/list/main/ctrl.js
+++ app/sponsorAds/list/main/ctrl.js
@@ -2,12 +2,12 @@ function ctrl(SponsorService) {
   var list = this;
 
   list.totalAds = 0;
-  //list.adsPerPage = 25; // this should match however many results your API puts on one page
   list.totalPages = new Array();
   list.currentPage = 1;
 
   list.pageChanged = function (newPage) {
     list_sponsors(newPage);
+    window.scrollTo(0, 100);
   };
 
   function list_sponsors(page) {
@@ -18,7 +18,6 @@ function ctrl(SponsorService) {
       for (var i = 1; i <= response.total_pages; i++) {
         list.totalPages.push(i);
       }
-
       list.currentPage = response.current_page;
 
     }).catch(function (err) {
@@ -26,7 +25,6 @@ function ctrl(SponsorService) {
     });
   }
   list_sponsors(1);
-
   list.actDect = function (id) {
     console.clear();
     SponsorService.updateStatus(id).then(function (response) {
@@ -42,7 +40,6 @@ function ctrl(SponsorService) {
       console.log('error: ' + err);
     });
   }
-
 }
 
 module.exports = ['SponsorService', ctrl];
