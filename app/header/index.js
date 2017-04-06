b0c9b1d347f81dd78ee5095b064efb885c53f1f8
Sanjay Singh <sanjays442@gmail.com>
2017-04-06 20:23:28 +0530

    created login page

--- app/header/index.js
+++ app/header/index.js
@@ -17,8 +17,37 @@ var internalLinks = [{
   uiSref: 'centerDetail',
   name: 'Add Listing'
 }, {
+  uiSref: 'logout',
+  name: 'Logout',
+  id: 'adic-logout'
+}, {
   uiSref: 'myProfile.index',
-  name: 'My Profile'
+  name: 'My Profile',
+  id: 'adic-my-profile'
+}];
+
+var internalLinksNoAuth = [{
+  uiSref: 'home',
+  name: 'Home'
+}, {
+  uiSref: 'treatmentCenterMap.index',
+  name: 'Treatment Centers'
+}, {
+  uiSref: 'aboutUs',
+  name: 'About Us'
+}, {
+  uiSref: 'blog',
+  name: 'Blog'
+}, {
+  uiSref: 'contactUs',
+  name: 'Contact Us'
+}, {
+  uiSref: 'centerDetail',
+  name: 'Add Listing'
+}, {
+  uiSref: 'login',
+  name: 'Login',
+  id: 'adic-login'
 }];
 
 var socialLinks = [{
@@ -32,13 +61,29 @@ var socialLinks = [{
   img: 'themes/addiction/images/gglplus.png'
 }];
 
-function HeaderCtrl() {
+function HeaderCtrl($log, $scope, $rootScope, localStorageService) {
   /* todo */
   this.socialLinks = socialLinks;
   this.internalLinks = internalLinks;
+  this.internalLinksNoAuth = internalLinksNoAuth;
+  $scope.$on('$stateChangeStart',
+    function (event, toState, toParams, fromState) {
+      var token = localStorageService.get('token');
+      if (token) {
+        $rootScope.login = 1;
+      } else {
+        var tostate = toState.name.split('.');
+        $log.error('tostate: ' + tostate[0] + ' -->' + fromState.name);
+        if (tostate[0] === 'myProfile') {
+          event.preventDefault();
+        }
+      }
+    });
 }
 
 module.exports = {
   template: require('./view.html'),
   controller: HeaderCtrl
 };
+
+HeaderCtrl.$inject = ['$log', '$scope', '$rootScope', 'localStorageService'];
