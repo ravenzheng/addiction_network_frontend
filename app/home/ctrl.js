526df1271c7ff0e83b65420662082a0469a0a391
Ratnam Yadav <ratnam.yadav@icloud.com>
2017-04-19 00:53:39 +0530

    slider code refactored

--- app/home/ctrl.js
+++ app/home/ctrl.js
@@ -1,14 +1,4 @@
-module.exports = ['SliderService', ctrl];
+module.exports = ctrl;
 
-function ctrl(service) {
-  var vm = this;
-  vm.$onInit = onInit;
-  vm.myInterval = 5000;
-  vm.noWrapSlides = false;
-  vm.active = 0;
-  function onInit() {
-    service.getSlider().then(function (result) {
-      vm.sliders = result.sliders;
-    });
-  }
+function ctrl() {
 }
