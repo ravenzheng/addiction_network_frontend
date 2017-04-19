3fe623c77535037dcdc1f1378d577587353fba9e
unknown <sanjays442@gmail.com>
2017-04-19 14:07:11 +0530

    Fixed issue122

--- app/myProfile/paymentDetails/edit/ctrl.js
+++ app/myProfile/paymentDetails/edit/ctrl.js
@@ -1,6 +1,22 @@
 function ctrl($log, $stateParams, $rootScope, Status, service) {
   var vm = this;
   var profileId = $stateParams.id;
+  var creditCardType = require('credit-card-type');
+  vm.detectCardType = function (card) {
+    if (angular.isDefined(card)) {
+      vm.cardType = creditCardType(card);
+      vm.totalDigits = card.length;
+    } else {
+      vm.cardType = null;
+      vm.totalDigits = 0;
+    }
+  };
+
+  var curYear = new Date().getFullYear();
+  vm.testYear = function () {
+    vm.curYear = curYear;
+  };
+  vm.middleName = '';
   vm.submit = function () {
     // validating file type
     vm.err_type = 0;
@@ -15,6 +31,9 @@ function ctrl($log, $stateParams, $rootScope, Status, service) {
     var formData = new FormData();
     var paymentData = {
       'card_no': vm.card,
+      'first_name': vm.firstName,
+      'middle_name': vm.middleName,
+      'last_name': vm.lastName,
       'expiry_year': vm.year,
       'expiry_month': vm.month,
       'card_code': vm.cvv
