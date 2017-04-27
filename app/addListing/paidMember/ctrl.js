46bb938255c32d385fdf9f2b182e67782c50304a
unknown <sanjays442@gmail.com>
2017-04-27 20:00:26 +0530

    Updated membership plan button action in addlisting

--- app/addListing/paidMember/ctrl.js
+++ app/addListing/paidMember/ctrl.js
@@ -16,6 +16,7 @@ function ctrl($rootScope, $log, $state, UIState) {
     $rootScope.addListingStepDone = 3;
     $rootScope.doneSteps = $rootScope.doneSteps.concat(['paidMember']);
     $rootScope.centerReset = 0;
+    $rootScope.showSteps = ['contactInfo', 'userInfo', 'paidMember', 'centerInfo', 'centerDetails', 'paymentDetails', 'sponsoredPage', 'bannerAd'];
     $state.go(UIState.ADD_LISTING.CENTER_INFO);
   };
   lm.freeSignup = function () {
