17e0beedc2bd02fc07b7f6926f5a4dc044fbdabb
Sanjay Singh <sanjays442@gmail.com>
2017-06-25 15:45:53 +0530

    added comment box

--- app/blog/single.js
+++ app/blog/single.js
@@ -1,14 +1,29 @@
-module.exports = ['$scope', 'BlogService', '$log', '$state', '$sce',
+module.exports = ['$scope', '$window', 'BlogService', '$log', '$state', '$sce',
   ctrl];
 
-function ctrl($scope, service, $log, $state, $sce) {
+function ctrl($scope, $window, service, $log, $state, $sce) {
   var vm = this;
   var $stateParams = $state.params;
   vm.singleBlog = $stateParams.single;
   service.getBlogSingle(vm.singleBlog).then(function (result) {
     vm.result = result.data;
     vm.content = $sce.trustAsHtml(result.data[0].post_content);
+    //  vm.html = $sce.trustAsHtml('<script>alert(testing)</script>');
   }).catch(function (err) {
     $log.info(err);
+  });
+  $scope.$on('$stateChangeSuccess', function () {
+    if (!$window.FB) {
+      (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {
+          return;
+        }
+        js = d.createElement(s);
+        js.id = id;
+        js.src = '//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+    }
   });
 }
