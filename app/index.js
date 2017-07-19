9c0eef14917a7a4a0a75b7c23c66c18a924550c3
Sanjay Singh <sanjays442@gmail.com>
2017-07-19 16:11:08 +0530

    generated xml sitemap

--- app/index.js
+++ app/index.js
@@ -41,7 +41,7 @@ angular.module(appName, [
         angular.element.get('http://192.185.67.241/blog/post_exist.php?slug=' + slug, function (data, status) {
           if (status === 'success') {
             if (data > 0) {
-              window.location.href = '/blog' + path;
+              window.location.href = '/blog' + slug;
             } else {
               // $urlRouterProvider.otherwise('/');
               window.location.href = '/';
