e034344e52287d46063d3130c4dcef92521cb36d
Sanjay Singh <sanjays442@gmail.com>
2017-07-26 22:45:04 +0530

    myprofile fixed

--- app/myProfile/upgradeAccount/ctrl.js
+++ app/myProfile/upgradeAccount/ctrl.js
@@ -3,8 +3,9 @@ module.exports = ['$state', 'UIState', '$rootScope', 'localStorageService', ctrl
 function ctrl($state, UIState, $rootScope, localStorageService) {
   var vm = this;
   var membershipType = localStorageService.get('membershipType', 'sessionStorage');
-  if (membershipType !== 'free') {
-    $state.go(UIState.MY_PROFILE.PAYMENT_DETAILS_ADD);
+  // alert(membershipType);
+  if (membershipType !== 'free' || membershipType !== null) {
+    // $state.go(UIState.MY_PROFILE.PAYMENT_DETAILS_ADD);
   }
   vm.payment = function (type) {
     $rootScope.membershipType = type;
