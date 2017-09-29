83e1decc3ab2a8997c5968ee18f261857b691c2a
sanjay <sanjays442@gmail.com>
2017-09-29 14:44:22 +0530

    new pages created

--- app/signUp/UserCreate/ctrl.js
+++ app/signUp/UserCreate/ctrl.js
@@ -1,26 +1,30 @@
-module.exports = ['$injector', '$scope', '$log', '$rootScope', '$state', 'UIState', 'SignUpService', ctrl];
+module.exports = ['$injector', '$scope', '$log', '$rootScope', '$state', 'UIState', 'SignUpService', 'Status', ctrl];
 
-function ctrl($injector, $scope, $log, $rootScope, $state, UIState, service) {
+function ctrl($injector, $scope, $log, $rootScope, $state, UIState, service, Status) {
   var vm = this;
   vm.userCreate = function () {
-    $state.go(UIState.SIGN_UP.USER_PROFILE);
+    var lm = $rootScope; // this;
     var formData = new FormData();
     var sigupData = {
       'first_name': vm.first_name,
       'last_name': vm.last_name,
-      'company_name': vm.company_name,
-      'phone_no': vm.phone_no,
+      'company': vm.company_name,
+      'phone': vm.phone_num,
       'email': vm.email,
-      'password': vm.password
+      'password': vm.password,
+      'username': vm.phone_num + vm.first_name
     };
 
     for (var key in sigupData) {
       formData.append('user[' + key + ']', sigupData[key]);
     }
+
     $log.info(formData);
-    service.signUp(sigupData).then(function (result) {
+    service.signUp(formData).then(function (result) {
+      $state.go(UIState.SIGN_UP.USER_PROFILE);
       $log.info(result);
     }).catch(function (err) {
+      lm.$emit(Status.FAILED, err.data.error);
       $log.info(err);
     });
   };
