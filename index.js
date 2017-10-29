40e3a842668ad3d9df4d16857f23317ad99d699b
Sanjay Singh <sanjays442@gmail.com>
2017-10-29 19:27:02 +0530

    prerender

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-var connect_s4a = require('connect-s4a');
-var token = "9b86e9430a14b8bab3a4cac92bc22520";
 var express = require('express');
+var snapsearch = require('snapsearch-client-nodejs');
 var app = express();
-app.use(connect_s4a(token));
-app.get('/', function (req, res) {
-  res.send('hello world');
+
+app.use(connect_s4a("9b86e9430a14b8bab3a4cac92bc22520"));
+
+// app.get('/', function (request, response) {
+//   response.send('Was not a robot and we are here inside app');
+// });
+
+app.set('port', (process.env.PORT || 3000));
+app.use(express.static(__dirname + '/build'));
+// app.use(require('prerender-node'));
+
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'));
 });
-app.listen(3000);
