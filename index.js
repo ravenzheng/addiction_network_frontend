a40783528b264f82dbaf480a874104c1e49b9936
Sanjay Singh <sanjays442@gmail.com>
2017-10-29 18:11:42 +0530

    prerender

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ app.configure(function () {
   // Here we require the prerender middleware that will handle requests from Search Engine crawlers
   // We set the token only if we're using the Prerender.io service
   app.use(require('prerender-node').set('prerenderToken', 'iza1QCOe9dkwSczOjldR'));
-  app.use(express.static("app"));
-  app.use(app.router);
+  //  app.use(express.static("app"));
+  // app.use(app.router);
 });
 
 app.listen(3000);
