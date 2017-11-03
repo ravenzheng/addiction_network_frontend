5e614a6e6fb354c37d9a803f2ca7b4d3f50ec497
Sanjay Singh <sanjays442@gmail.com>
2017-11-03 19:43:00 +0530

    live seo

--- app/index.js
+++ app/index.js
@@ -12,7 +12,7 @@ angular.module(appName, [
   require('./advertisement'),
   require('./myProfile'),
   require('./featuredTreatmentCenter'),
-  // require('./addListing'),
+  require('./addListing'),
   require('./aboutUs'),
   require('./contactUs'),
   require('./contactThank'),
@@ -34,7 +34,7 @@ angular.module(appName, [
   .constant('endPoint', require('./endPoint'))
   .config(['$locationProvider', function ($locationProvider) {
     $locationProvider.hashPrefix('');
-    // $locationProvider.html5Mode(true);
+    $locationProvider.html5Mode(true);
   }])
   .config(['$urlRouterProvider', function ($urlRouterProvider) {
     // .when('/customers', route.resolve('Customers'))
