c47e7571c249fbc36df388fdba780c2cb607c137
Sanjay Singh <sanjays442@gmail.com>
2017-12-04 16:13:32 +0530

    frontend

--- app/signUp_old/myProfile/ctrl.js
+++
@@ -1,8 +0,0 @@
-module.exports = ['$injector', '$scope', '$log', '$rootScope', '$state', 'UIState', ctrl];
-
-function ctrl($injector, $scope, $log, $rootScope, $state, UIState) {
-  var vm = this;
-  vm.signUp = function () {
-    $state.go(UIState.SIGN_UP.USER_CREATE);
-  };
-}
